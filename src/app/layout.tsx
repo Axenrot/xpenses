26dd6a9d9a679996e8cae4d635583d01958dc469
Leon <yurileon@hotmail.com.br>
2023-10-03 14:58:42 -0300

    responsivity fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} h-screen flex bg-black`}>
+      <body className={`${inter.className} h-screen flex p-3 bg-black`}>
         <Container>{children}</Container>
       </body>
     </html>
